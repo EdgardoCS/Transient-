dc75aa808c594bd2020bf7b8ec4791c28c46a6da
EdgardoCS <e.silva.plaza@gmail.com>
2019-05-13 15:44:08 -0400

    PIL algorythm

---
+++ code/Py/loadImage.py
@@ -0,0 +1,52 @@
+import time
+import os
+
+
+from PIL import Image, ExifTags
+
+cwd = os.path.dirname(os.path.abspath(__file__))
+print(cwd)
+
+
+for i in range(1, 300):
+    index = i
+    #path = './img/G02-%g.jpg' % index
+    try:
+        path = '../../../capas_sistam/G09B/IMG/G09B-%g.jpg' % index
+        counter = 0
+        try:
+            image = Image.open(path)
+            print('editing...', path)
+            exif = {
+                ExifTags.TAGS[k]: v
+                for k, v in image._getexif().items()
+                if k in ExifTags.TAGS
+            }
+            print(exif)
+
+            print('Orientation', exif['Orientation'])
+            #try:
+
+            if exif['Orientation'] == 3:
+                image = image.rotate(180, expand=True)
+                print('image change', index)
+            elif exif['Orientation'] == 6:
+                image = image.rotate(270, expand=True)
+                print('image change', index)
+            elif exif['Orientation'] ==8:
+                image = image.rotate(90, expand=True)
+                print('image change', index)
+
+            image.save(path)
+            image.close()
+            time.sleep(1)
+            #except:
+             #   print('no orientation field')
+              #  pass
+        except AttributeError:
+            #print(AttributeError)
+            #time.sleep(1)
+            pass
+    except:
+        #print('file not found')
+        pass
