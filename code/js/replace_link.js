fb59cdb1fcfc2387829a8a91318a783ba621e9c6
EdgardoCS <e.silva.plaza@gmail.com>
2019-05-22 11:35:39 -0400

    lev_v1

--- code/js/replace_link.js
+++ code/js/replace_link.js
@@ -1,16 +1,18 @@
 const fs = require('fs')
 const path = '/../../../capas_sistam/';
-const file1 = fs.readFileSync(__dirname + path + 'G01/GEOJSON/G01_SECTORESPERANZA.geojson', 'utf8')
-const file2 = fs.readFileSync(__dirname + path + 'G01/GEOJSON/G01.json', 'utf8')
+// const file1 = fs.readFileSync(__dirname + path + 'G01/GEOJSON/G01_SECTORESPERANZA.geojson', 'utf8')
 
+const file1 = fs.readFileSync('../../levantamiento.geojson', 'utf8')
+const file2 = fs.readFileSync('../../levantamiento_completo.json', 'utf8')
 
-save_file = function(data1){
-    fs.writeFile("G01.json", JSON.stringify(data1), function(err) {
+save_file = function (data1) {
+    fs.writeFile("transversal.json", JSON.stringify(data1), function (err) {
         if (err) {
             console.log(err);
         }
     });
 }
+
 try {
     const data1 = JSON.parse(file1)
     const data2 = JSON.parse(file2)
@@ -20,14 +22,20 @@ try {
 
     for (let i in feats) {
         let id = feats[i].properties.ID_FINAL;
-        let temp_photo = feats[i].properties.FOTO_LNK;
-        let photo = temp_photo.split('"')[1]
+        // let temp_photo = feats[i].properties.FOTO_LNK;
+        // let photo = temp_photo.split('"')[1]
+        let layer = feats[i].properties.layer.split("_")[0];
 
         for (let u in img) {
             var target_id = img[u].id.split('-')[1].split('.')[0]
             var target_link = img[u].link
-            if (id == target_id) {
-                data1.features[i].properties.FOTO_LNK = target_link; 
+            var target_sector = img[u].sector;
+
+            if (target_sector == layer) {
+                if (id == target_id) {
+                    data1.features[i].properties.FOTO_LNK = target_link;
+                    console.log('match at ' + i)
+                }
             }
         }
     }
@@ -35,4 +43,3 @@ try {
 } catch (err) {
     console.error(err)
 }
-
